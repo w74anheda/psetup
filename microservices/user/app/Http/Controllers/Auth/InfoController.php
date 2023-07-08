45e5bd13d819759084c944d3ec69eef04f9e6fd1
masoud nazarpoor <m74asoud@gmail.com>
2023-07-08 21:02:05 +0330

    fix api me and add permissions query with union

--- microservices/user/app/Http/Controllers/Auth/InfoController.php
+++ microservices/user/app/Http/Controllers/Auth/InfoController.php
@@ -14,27 +14,10 @@ class InfoController extends Controller
     public function me(Request $request)
     {
         $user = $request->user();
-
         return response(
-            $this->makeUserWithWithCombinedPermissions($user),
+            [ ...$user->toArray(), 'permissions' => $user->allPermissions()->get() ],
             Response::HTTP_OK
         );
     }
 
-    private function getAllPermissionsCombineRolesPermissions(User $user)
-    {
-        $user->load([ 'permissions', 'roles.permissions' ]);
-        $permissions          = $user->permissions->pluck('name');
-        $roles_permissions    = $user->roles->map(fn($role) => $role->permissions()->pluck('name'))->flatten();
-        $combined_permissions = $permissions->merge($roles_permissions);
-        return [ ...$combined_permissions->unique()->toArray() ];
-    }
-
-    private function makeUserWithWithCombinedPermissions(User $user): array
-    {
-        $permissions = $this->getAllPermissionsCombineRolesPermissions($user);
-        $user->makeHidden([ 'roles', 'permissions' ]);
-        $user = array_merge($user->toArray(), [ 'permissions' => $permissions ]);
-        return $user;
-    }
 }
--- microservices/user/app/Services/Acl/HasPermission.php
+++ microservices/user/app/Services/Acl/HasPermission.php
@@ -6,6 +6,7 @@ namespace App\Services\Acl;
 use App\Models\Permission;
 use App\Models\Role;
 use App\Models\User;
+use DB;
 use Illuminate\Support\Arr;
 
 trait HasPermission
@@ -20,6 +21,29 @@ trait HasPermission
             case $this instanceof Role:
                 return $this->belongsToMany(Permission::class, 'roles_permissions');
         }
+    }
+
+    public function allPermissions()
+    {
+        if(!$this instanceof User) return null;
+
+        $a = DB::table('users')
+        ->select('permissions.id as id', 'permissions.name as name')
+        ->join('users_permissions','users.id','=','users_permissions.user_id')
+        ->join('permissions','permissions.id','=','users_permissions.user_id')
+        ->where(['user_id' => $this->id])
+        ->distinct();
+
+        $b = DB::table('users')
+        ->select('permissions.id as id', 'permissions.name as name')
+        ->join('users_roles','users.id','=','users_roles.user_id')
+        ->join('roles','users_roles.role_id','=','roles.id')
+        ->join('roles_permissions','roles.id','=','roles_permissions.role_id')
+        ->join('permissions','permissions.id','=','roles_permissions.permission_id')
+        ->where(['user_id' => $this->id])
+        ->distinct();
+
+        return $a->union($b)->orderBy('id');
     }
 
     private function getAllPermissions(string...$permissions_name)

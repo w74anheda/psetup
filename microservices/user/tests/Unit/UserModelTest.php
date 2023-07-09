418c56c8ff203a332904f367abcee6697c3c78d2
masoud nazarpoor <m74asoud@gmail.com>
2023-07-09 22:43:44 +0330

    add user model test and add mysql  test databaset

--- microservices/user/tests/Unit/UserModelTest.php
+++ microservices/user/tests/Unit/UserModelTest.php
@@ -2,15 +2,40 @@
 
 namespace Tests\Unit;
 
+use App\Console\Kernel;
 use App\Models\User;
-use PHPUnit\Framework\TestCase;
+use App\Models\UserIp;
+use Carbon\Carbon;
+use DB;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+// use Illuminate\Foundation\Testing\TestCase;
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+
 
 class UserModelTest extends TestCase
 {
 
-    public function test_check_attributes()
+    public function createApplication(): Application
     {
-        $user       = User::factory()->make();
+        $app = require __DIR__ . '/../../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+
+    public function setup(): void
+    {
+        parent::setUp();
+        // $this->artisan('migrate:fresh --seed');
+    }
+
+    public function test_check_user_attributes()
+    {
+        $user       = User::factory()->create();
         $attributes = [
             'first_name',
             'last_name',
@@ -31,23 +56,47 @@ class UserModelTest extends TestCase
     }
 
     /**
-     * @depends test_check_attributes
-     */
-    public function test_check_is_new_and_is_active_and_is_completed($user)
-    {
-        $this->assertTrue($user->is_new);
-        $this->assertTrue($user->isNew());
-        $this->assertFalse($user->is_active);
-        $this->assertFalse($user->isActive());
-        $this->assertFalse($user->personal_info['is_completed']);
-        return $user;
-    }
-
-    /**
-     * @depends test_check_is_new_and_is_active_and_is_completed
+     * @depends test_check_user_attributes
      */
     public function test_check_user_factory($user)
     {
-        dump($user->first_name);
+        $this->assertIsString($user->first_name);
+        $this->assertIsString($user->last_name);
+        $this->assertContains($user->gender, [ 'male', 'female', 'both' ]);
+        $this->assertIsString($user->phone);
+        $this->assertTrue(is_null($user->activated_at));
+        $this->assertTrue(!!filter_var($user->email, FILTER_VALIDATE_EMAIL));
+        $this->assertTrue(is_null($user->email_verified_at));
+        $this->assertFalse($user->is_active);
+        $this->assertTrue($user->is_new);
+        $this->assertTrue(!!filter_var($user->registered_ip, FILTER_VALIDATE_IP));
+        $this->assertTrue(is_array($user->personal_info));
+        $this->assertArrayHasKey('is_completed', $user->personal_info);
+        $this->assertArrayHasKey('birth_day', $user->personal_info);
+        $this->assertArrayHasKey('is_completed', $user->personal_info);
+        $this->assertArrayHasKey('national_id', $user->personal_info);
+        $this->assertFalse($user->personal_info['is_completed']);
+        $this->assertTrue(is_null($user->personal_info['birth_day']));
+        $this->assertTrue(is_null($user->personal_info['national_id']));
+    }
+
+    /**
+     * @depends test_check_user_attributes
+     */
+    public function test_user_check_setLastOnlineAt($user)
+    {
+        $datetime = today()->subHours(10);
+        $this->assertTrue(is_null($user->last_online_at));
+        $user->setLastOnlineAt($datetime);
+        $this->assertTrue($datetime->eq($user->last_online_at));
+    }
+
+    /**
+     * @depends test_check_user_attributes
+     */
+    public function test_user_relations($user)
+    {
+        $this->assertTrue($user->ips() instanceof HasMany);
+        $this->assertTrue($user->ips()->getModel() instanceof UserIp);
     }
 }

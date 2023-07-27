8f2f2cb9343dbfe3d82772426932724306312f45
masoud nazarpoor <m74asoud@gmail.com>
2023-07-27 22:18:08 +0330

    refactor

--- microservices/user/app/Models/City.php
+++ microservices/user/app/Models/City.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 use App\Models\Traits\HasAddress;
+use App\Presenters\PresentAble;
+use App\Presenters\Presenter;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Presenters\City\Api as CityApiPresenter;
 
 class City extends Model
 {
-    use HasFactory, HasAddress;
+    use HasFactory, HasAddress, PresentAble;
 
     protected $fillable = [ 'state_id', 'name' ];
 
@@ -17,6 +20,11 @@ class City extends Model
     public function state()
     {
         return $this->belongsTo(State::class);
+    }
+
+    protected function presenterHandler(): Presenter
+    {
+        return new CityApiPresenter($this);
     }
 
 }
